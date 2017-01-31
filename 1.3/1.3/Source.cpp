15ae66d5e1ac29e879d30990f302873159f7eb0b
game110pm <game110pm@605-110-STU13>
2017-01-31 12:43:23 -0700

    this is an awesome joke

---
+++ 1.3/1.3/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "What did one casket ask the other sick casket?" << endl;
+	cout << "Is that you, coffin?   HAHAHA" << endl;
+
+
+
+}
